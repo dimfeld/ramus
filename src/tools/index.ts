4a7455092cdbc0ef83ecb2753dadb981b23a85b9
Daniel Imfeld <daniel@imfeld.dev>
2024-05-08 16:55:46 -1000

    implement a bit of wikipedia tool

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,1 +1,10 @@
 export * from './brave_search.js';
+import type { Schema } from 'jsonschema';
+
+export interface ToolConfig {
+  name: string;
+  description: string;
+  schema: Schema;
+  // TODO need to define some output
+  run(input: any): Promise<any>;
+}
--- src/tools/wikipedia.ts
+++ src/tools/wikipedia.ts
@@ -1,2 +1,40 @@
-// TODO
-// Extract text, infoboxes, other relevant stuff
+import * as wtf from 'wtf_wikipedia';
+// @ts-expect-error no types available
+import wtf_disambig from 'wtf-plugin-disambig';
+wtf.extend(wtf_disambig);
+
+async function getByTitle(title: string) {
+  let doc = await wtf.fetch(title, { 'Api-User-Agent': 'Orchard/1.0', lang: 'en', title });
+  if (Array.isArray(doc)) {
+    // Shouldn't return an array when we're just fetching 1 URL, but just in case
+    return doc[0];
+  }
+
+  return doc;
+}
+
+export async function getWikipediaPage(title: string, section?: string) {
+  let doc = getByTitle(title);
+
+  // @ts-expect-error No types for disambig plugin
+  let disambig = doc.disambig();
+
+  if (disambig) {
+    if (section) {
+      // @ts-expect-error no types
+      let first = disambig.pages.find((p) => p.section === section);
+      if (first) {
+        return getByTitle(first.link);
+      }
+    } else {
+      return getByTitle(disambig.pages[0].link);
+    }
+  } else {
+    return doc;
+  }
+}
+
+export async function getWikipediaInfoBox(title: string) {
+  const doc = await getWikipediaPage(title);
+  return doc?.infobox()?.keyValue();
+}
